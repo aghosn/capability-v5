44784075e6d87d17e5cf509b9b43a8db6f16cf75
Adrien Ghosn <ghosn.adrien@gmail.com>
2025-04-24 19:25:31 +0100

    Increasing coverage

--- capability_engine/tests/domain.rs
+++ capability_engine/tests/domain.rs
@@ -3,6 +3,7 @@ use capa_engine::domain::*;
 use capa_engine::memory_region::{
     Access, Attributes, MemoryRegion, RegionKind, Remapped, Rights, Status as MStatus,
 };
+use capa_engine::Engine;
 use std::cell::RefCell;
 use std::rc::Rc;
 
@@ -26,6 +27,33 @@ fn create_root() -> Capability<MemoryRegion> {
     })
 }
 
+fn create_root_region() -> Capability<MemoryRegion> {
+    Capability::<MemoryRegion>::new(MemoryRegion {
+        kind: RegionKind::Carve,
+        status: MStatus::Exclusive,
+        access: Access::new(0, 0x10000, Rights::READ | Rights::WRITE | Rights::EXECUTE),
+        attributes: Attributes::NONE,
+        remapped: Remapped::Identity,
+    })
+}
+
+fn setup_engine_with_root() -> (
+    Engine,
+    CapaRef<Domain>,
+    CapaRef<MemoryRegion>,
+    LocalCapa, // ref_region returned by `add_root_region`
+) {
+    let engine = Engine::new();
+    let root_domain = create_root_domain();
+    let root_region = create_root_region();
+
+    let ref_td = Rc::new(RefCell::new(root_domain));
+    let ref_mem = Rc::new(RefCell::new(root_region));
+    let ref_region = engine.add_root_region(&ref_td, &ref_mem).unwrap();
+
+    (engine, ref_td, ref_mem, ref_region)
+}
+
 #[test]
 fn test_empty_root_domain() {
     let domain = create_root_domain();
@@ -44,4 +72,104 @@ fn test_root_domain_with_root_memory() {
     let display_output = format!("{}", domain);
     let expected_output = format!("td0 = Sealed domain(r0)\n|cores: 0xffffffffffffffff\n|mon.api: 0x1fff\n|vec0-255: ALLOWED|VISIBLE, r: 0x0, w: 0x0\nr0 = Exclusive 0x0 0x10000 with RWX mapped Identity\n");
     assert_eq!(display_output, expected_output);
+}
+
+#[test]
+fn test_unallowed_calls() {
+    let (engine, td0, _r0, td0_r0) = setup_engine_with_root();
+
+    let child_td = engine
+        .create(
+            td0.clone(),
+            1,
+            MonitorAPI::ATTEST,
+            InterruptPolicy::default_none(),
+        )
+        .unwrap();
+
+    // Let's seal the child directly.
+    engine.seal(td0.clone(), child_td).unwrap();
+
+    // Now try to send it something.
+    let access = Access::new(0x0, 0x1000, Rights::all());
+    let alias = engine.alias(td0.clone(), td0_r0, &access).unwrap();
+
+    let res = engine.send(td0.clone(), child_td, alias, Remapped::Identity);
+    assert!(res.is_err());
+
+    engine.revoke(td0.clone(), child_td, 0).unwrap();
+
+    // Now try to make the child do stuff while not sealed.
+    let child_td = engine
+        .create(
+            td0.clone(),
+            1,
+            MonitorAPI::all(),
+            InterruptPolicy::default_none(),
+        )
+        .unwrap();
+
+    engine
+        .send(td0.clone(), child_td, alias, Remapped::Identity)
+        .unwrap();
+
+    {
+        let child = td0
+            .borrow()
+            .data
+            .capabilities
+            .get(&child_td)
+            .unwrap()
+            .as_domain()
+            .unwrap();
+
+        let access = Access::new(0x0, 0x1000, Rights::READ);
+        let res = engine.alias(child.clone(), 1, &access);
+        assert_eq!(res, Err(CapaError::DomainUnsealed));
+    }
+
+    engine.revoke(td0.clone(), child_td, 0).unwrap();
+
+    // Okay last one.
+    let child_td = engine
+        .create(
+            td0.clone(),
+            1,
+            MonitorAPI::CARVE,
+            InterruptPolicy::default_none(),
+        )
+        .unwrap();
+
+    let alias = engine.alias(td0.clone(), td0_r0, &access).unwrap();
+    engine
+        .send(td0.clone(), child_td, alias, Remapped::Identity)
+        .unwrap();
+    engine.seal(td0.clone(), child_td).unwrap();
+
+    {
+        let child = td0
+            .borrow()
+            .data
+            .capabilities
+            .get(&child_td)
+            .unwrap()
+            .as_domain()
+            .unwrap();
+
+        let res = engine.alias(child.clone(), 1, &Access::new(0x0, 0x1000, Rights::READ));
+        assert_eq!(res, Err(CapaError::CallNotAllowed));
+
+        let res = child.borrow().seal(1);
+        assert!(res.is_err());
+    }
+
+    // Okay try to reseal the same domain.
+    let res = engine.seal(td0.clone(), child_td);
+    assert_eq!(res, Err(CapaError::DomainSealed));
+
+    // Try to seal something wrong.
+    let res = engine.seal(td0.clone(), td0_r0);
+    assert_eq!(res, Err(CapaError::WrongCapaType));
+    let res = td0.borrow().seal(td0_r0);
+    assert_eq!(res, Err(CapaError::WrongCapaType));
 }
