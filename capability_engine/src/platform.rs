53610063db1e7fa0a0054e570674d64bba54a55c
Adrien Ghosn <ghosn.adrien@gmail.com>
2025-04-25 12:20:21 +0100

    Saving for now

--- capability_engine/src/platform.rs
+++ capability_engine/src/platform.rs
@@ -1,4 +1,7 @@
+use crate::capability::CapaError;
+
 pub trait Platform {
-    fn set_register();
-    fn get_register();
+    fn set_register(dom: u64, core: usize, field: usize, value: usize);
+    fn get_register(dom: u64, core: usize, field: usize) -> Result<usize, CapaError>;
+    fn get_interrupt(dom: u64, core: usize) -> Result<usize, CapaError>;
 }
